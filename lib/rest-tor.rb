f675df0cc08ec84e53e8dd3fe187d36833c41759
plusor <plusor@qq.com>
2017-12-15 21:21:57 +0800

    logger format

--- lib/rest-tor.rb
+++ lib/rest-tor.rb
@@ -7,6 +7,10 @@ Dir.glob(File.expand_path('../rest_tor/**/*.rb', __FILE__)).each { |file| requir
 
 module Tor
   def self.logger
-    Logger.new(STDOUT)
+    Logger.new(STDOUT).tap do |log|
+      log.formatter = proc do |severity, datetime, progname, msg|
+        "[#{datetime.strftime('%Y-%m-%d %H:%M:%S.%3N')}] [#{Process.pid}-#{Thread.current.object_id}] #{msg}\n"
+      end
+    end
   end
 end
