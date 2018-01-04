a0df143cd22e73ef0e6fd9d244cdfe163ccf1e19
plusor <plusor@qq.com>
2018-01-04 15:32:35 +0800

    custom logger

--- lib/rest-tor.rb
+++ lib/rest-tor.rb
@@ -7,10 +7,14 @@ Dir.glob(File.expand_path('../rest_tor/**/*.rb', __FILE__)).each { |file| requir
 
 module Tor
   def self.logger
-    Logger.new(STDOUT).tap do |log|
+    @logger ||= Logger.new(STDOUT).tap do |log|
       log.formatter = proc do |severity, datetime, progname, msg|
         "[#{datetime.strftime('%Y-%m-%d %H:%M:%S.%3N')}] [#{Process.pid}-#{Thread.current.object_id}] #{msg}\n"
       end
     end
+  end
+
+  def self.logger=(log)
+    @logger = log
   end
 end
